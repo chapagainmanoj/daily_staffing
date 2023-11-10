e88c72070c5eb17c56a8fede1a2d7426ffcb55d4
Manoj <chapagainmanoj35@gmail.com>
2023-11-10 01:19:56 -0500

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,32 @@ module.exports = {
     title: `daily_staffing`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
+  plugins: ["gatsby-transformer-remark", 
+  // {
+  //   resolve: 'gatsby-source-filesystem',
+  //   options: {
+  //     "name": "pages",
+  //     "path": "./src/pages/"
+  //   },
+  //   __key: "pages"
+  // },
+  {
+    resolve: "gatsby-plugin-google-gtag",
     options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+      trackingIds: ["UA-126570298-1"],
+    }
+  },
+  {
+  resolve: `gatsby-plugin-manifest`,
+  options: {
+    name: `Gatsby Starter Landing Page`,
+    short_name: `Gatsby Starter Landing Page`,
+    start_url: `/`,
+    background_color: `#fff`,
+    theme_color: `#000`,
+    display: `browser`,
+    icon: `src/assets/gatsby-monogram.png`,
+  },
+}
+]
 };
