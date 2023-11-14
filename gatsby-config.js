4a90265dacfd2c6a0c5f43da12b058316ff570aa
Manoj <chapagainmanoj35@gmail.com>
2023-11-14 12:42:21 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,31 +7,31 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: ["gatsby-transformer-remark", 
-  // {
-  //   resolve: 'gatsby-source-filesystem',
-  //   options: {
-  //     "name": "pages",
-  //     "path": "./src/pages/"
-  //   },
-  //   __key: "pages"
-  // },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  },
   {
     resolve: "gatsby-plugin-google-gtag",
     options: {
       trackingIds: ["UA-126570298-1"],
     }
   },
-  {
-  resolve: `gatsby-plugin-manifest`,
-  options: {
-    name: `Gatsby Starter Landing Page`,
-    short_name: `Gatsby Starter Landing Page`,
-    start_url: `/`,
-    background_color: `#fff`,
-    theme_color: `#000`,
-    display: `browser`,
-    icon: `src/assets/gatsby-monogram.png`,
-  },
-}
+  // {
+  // resolve: `gatsby-plugin-manifest`,
+  // options: {
+  //   name: `Gatsby Starter Landing Page`,
+  //   short_name: `Gatsby Starter Landing Page`,
+  //   start_url: `/`,
+  //   background_color: `#fff`,
+  //   theme_color: `#000`,
+  //   display: `browser`,
+  //   icon: `src/assets/gatsby-monogram.png`,
+  // },
+// }
 ]
 };
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import * as React from "react"
+import {
+  SuperHero
+} from "./../gatsby-theme-landing-page/sections/index"
+
+export default function Component () {
+  return <SuperHero/>
+}
